e826c44018f1125cf83271dca37e237aa03a795f
Mykhailo Pinchuk <kingmetok@gmail.com>
2022-05-27 12:51:33 +0400

    Implemented all except of PutTill

--- cache.go
+++ cache.go
@@ -3,21 +3,29 @@ package cache
 import "time"
 
 type Cache struct {
+	m map[string]string
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{make(map[string]string)}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	s, ok := c.m[key]
+	return s, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.m[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	var result []string
+	for _, vl := range c.m {
+		result = append(result, vl)
+	}
+	return result
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
 }
